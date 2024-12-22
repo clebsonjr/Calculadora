decf92b6b51886409a76622798ddcb1a98bd6ac8
clebs <clebs@DESKTOP-JK957L9>
2024-12-22 09:54:49 -0300

    Primeiro commit

---
+++ Calculadora/src/calculadora/java/Calculadora.java
@@ -0,0 +1,39 @@
+package calculadora.java;
+
+import java.util.Scanner;
+
+public class Calculadora {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Digite o primeiro número: ");
+		double num1 = scanner.nextDouble();
+		System.out.print("Digite o segundo número: ");
+		double num2 = scanner.nextDouble();
+		System.out.print("Digite a operação (+, -, *, /): ");
+		char operador = scanner.next().charAt(0);
+		scanner.close();/*Fecha o Scanner para liberar recursos.*/
+		double resultado;
+
+		switch (operador) {
+		case '+':
+			resultado = num1 + num2;
+			break;
+		case '-':
+			resultado = num1 - num2;
+			break;
+		case '*':
+			resultado = num1 * num2;
+			break;
+		case '/':
+			resultado = num1 / num2;
+			break;
+		default:
+			System.out.println("Operação inválida.");
+			return;
+		}
+
+		System.out.println(num1 + " " + operador + " " + num2 + ": " + resultado);
+	}
+
+}
